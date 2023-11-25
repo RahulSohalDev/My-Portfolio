a59fb87a482cc107738e90718b813ea44666af73
Rahul Sohal <dhiman7571@gmail.com>
2023-11-26 00:55:42 +0530

    Create Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Location from "../assets/contact/map_1865269.png";
+import Gmail from "../assets/contact//gmail_732200.png";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="contact">
+      <div className="contact-text">
+        <h3>CONTACT</h3>
+        <h2>Don't be shy! Hit me up!</h2>
+      </div>
+      <div className="location-mail">
+        <div className="location">
+          <img src={Location} alt="" />
+          <div className="location-link">
+            <h4>Location</h4>
+            <p>Rajpura, Punjab</p>
+          </div>
+        </div>
+        <div className="mail">
+        <img src={Gmail} alt="" />
+          <div className="mail-link">
+            <h4>Mail</h4>
+            <p>dhiman7571@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
